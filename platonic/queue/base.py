2875fe2fc2d30002324318616e0957fee817771c
Anatoly Scherbakov <altaisoft@gmail.com>
2020-10-17 20:47:13 +0700

    #1 update code

---
+++ platonic/queue/base.py
@@ -0,0 +1,20 @@
+from abc import ABC
+from functools import cached_property
+from typing import Generic, Type
+
+from typecasts import Typecasts
+
+from platonic.type_args import generic_type_args
+from platonic.queue import ValueType, InternalType
+
+
+class BaseQueue(ABC, Generic[ValueType]):
+    """Base class for queues."""
+
+    internal_type: Type[InternalType]
+    typecasts: Typecasts
+
+    @cached_property
+    def value_type(self) -> Type[ValueType]:
+        """Extract the type of queue message from type args."""
+        return generic_type_args(self)[0]
---
+++ platonic/queue/errors.py
@@ -0,0 +1,75 @@
+import dataclasses
+from typing import Generic
+
+from documented import DocumentedError
+
+from platonic.queue.input import Message, InputQueue
+from platonic.queue.types import ValueType
+from platonic.queue.base import BaseQueue
+
+
+@dataclasses.dataclass
+class MessageReceiveTimeout(DocumentedError):
+    """
+    No messages received within {self.timeout} {self.readable_time_suffix}.
+
+      Queue: {self.queue}
+    """
+
+    queue: InputQueue
+    timeout: int
+
+    @property
+    def readable_time_suffix(self) -> str:
+        """Second or seconds."""
+        if self.timeout == 1:
+            return 'second'
+        else:
+            return 'seconds'
+
+
+@dataclasses.dataclass
+class QueueDoesNotExist(DocumentedError, Generic[ValueType]):
+    """Specified queue does not exist."""
+
+    queue: BaseQueue[ValueType]
+
+
+@dataclasses.dataclass
+class MessageDoesNotExist(DocumentedError, Generic[ValueType]):
+    """Specified message {self.message} does not exist in the queue."""
+
+    message: Message[ValueType]
+    queue: InputQueue[ValueType]
+
+
+@dataclasses.dataclass
+class MessageTooLarge(DocumentedError):
+    """
+    Message is too large.
+
+    Provided message size is {self.message_size} bytes, while
+    maximum message size is {self.max_supported_size} bytes.
+
+    Message preview:
+        {self.message_head}...{self.message_tail}
+    """
+
+    max_supported_size: int
+    message_body: str
+
+    message_preview_size: int = 10
+
+    @property
+    def message_head(self):
+        """First N bytes of the message."""
+        return self.message_body[:self.message_preview_size]
+
+    @property
+    def message_tail(self):
+        """Last N bytes of the message."""
+        return self.message_body[-self.message_preview_size:]
+
+    @property
+    def message_size(self) -> int:
+        return len(self.message_body)
